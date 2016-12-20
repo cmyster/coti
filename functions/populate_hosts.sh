c03ee3041cd3e8aa32a9e9d983d4b2e305e63d11
cmyster <amit.ugol@gmail.com>
2016-12-20 15:31:05 +0200

    Adds all hosts into hosts

---
+++ functions/populate_hosts.sh
@@ -0,0 +1,15 @@
+populate_hosts ()
+{
+    HOST_NAME=$1
+    cat > populate_hosts <<EOF
+#!/bin/bash
+cd /home/stack
+source stackrc
+openstack server list -f value \\
+  | awk '{print \$4" "\$2}' \\
+  | sed 's/ctlplane=//g' \\ 
+  | sed 's/overcloud-//g' \\
+  | sort -rk2 >> /etc/hosts
+EOF
+    run_script_file populate_hosts stack $HOST_NAME /home/stack/
+}

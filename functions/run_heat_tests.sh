eb3ee99bba72a71bc7df9bfaffdd3e5009617c52
cmyster <augol@redhat.com>
2017-01-03 11:23:24 +0200

    Moving heat tests results to local work folder.

--- functions/run_heat_tests.sh
+++ functions/run_heat_tests.sh
@@ -3,4 +3,5 @@ run_heat_tests ()
     HOST_NAME=$1    
     cp $CWD/functions/run_heat_tests .
     run_script_file run_heat_tests root $HOST_NAME /root/
+    scp root@${HOST_NAME}:/root/heat_integrationtests.xml .
 }

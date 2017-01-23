9ac88babe418afcd9432c3a78b564dc1a1f858bf
root <root@seal56.qa.lab.tlv.redhat.com>
2017-01-23 15:12:19 +0200

    Added hiera rule to put selinux on permissive.

--- functions/undercloud_install.sh
+++ functions/undercloud_install.sh
@@ -9,6 +9,8 @@ if [[ "$UNDER_SEL" != "enforcing" ]]
 then
     /usr/bin/sudo /usr/bin/sed -i "s/SELINUX=.*/SELINUX=$UNDER_SEL/" /etc/selinux/config
     /usr/bin/sudo /usr/sbin/setenforce 0
+    sed -i '/hieradata_override/d' /home/stack/undercloud.conf
+    echo "hieradata_override = $CWD/templates/hiera_selinux.yaml" >> /home/stack/undercloud.conf
 fi
 wget -q -nv -nd -np -r -A tar ${TAR_PATH}/ || exit 1
 tar xf images.tar

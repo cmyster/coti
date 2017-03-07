67b757035d76521b323dc24f237c718a6627feb5
root <root@seal56.qa.lab.tlv.redhat.com>
2017-03-07 10:19:02 +0200

    Fixed selinux on the undercloud.

--- functions/undercloud_install.sh
+++ functions/undercloud_install.sh
@@ -3,6 +3,7 @@ undercloud_install ()
     HOST=$1
     echo "Installing openstack undercloud."
     scp -q $CWD/undercloud.conf stack@$HOST:
+    scp -q $CWD/templates/hiera_selinux.yaml stack@$HOST:
     cat > install <<EOF
 cd /home/stack/
 if [[ "$UNDER_SEL" != "enforcing" ]]
@@ -10,7 +11,7 @@ then
     /usr/bin/sudo /usr/bin/sed -i "s/SELINUX=.*/SELINUX=$UNDER_SEL/" /etc/selinux/config
     /usr/bin/sudo /usr/sbin/setenforce 0
     sed -i '/hieradata_override/d' /home/stack/undercloud.conf
-    echo "hieradata_override = $CWD/templates/hiera_selinux.yaml" >> /home/stack/undercloud.conf
+    echo "hieradata_override = /home/stack/hiera_selinux.yaml" >> /home/stack/undercloud.conf
 fi
 wget -q -nv -nd -np -r -A tar ${TAR_PATH}/ || exit 1
 tar xf images.tar

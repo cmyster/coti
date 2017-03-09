301fa3497dcab5c1b41a73188479bc0b1f4bac17
root <root@seal56.qa.lab.tlv.redhat.com>
2017-03-09 06:44:29 +0200

    Changed criteria to determine undercloud installation.

--- functions/undercloud_install.sh
+++ functions/undercloud_install.sh
@@ -16,9 +16,18 @@ fi
 wget -q -nv -nd -np -r -A tar ${TAR_PATH}/ || exit 1
 tar xf images.tar
 sudo yum remove -y *bigswitch*
-openstack undercloud install || exit 1
+openstack undercloud install
+tail -n 20 .instack/install-undercloud.log | grep "install complete" &> /dev/null
+
+if [ $? -ne 0 ]
+then
+    echo "Installation did not report complete."
+    exit 1
+fi
+
 if [ ! -r /home/stack/stackrc ]
 then
+    echo "No stackrc file was generated."
     exit 1
 fi
 EOF

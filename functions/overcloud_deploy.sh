b8d1546c73f61759cd67ed079f879359b278b43a
root <root@seal56.qa.lab.tlv.redhat.com>
2017-05-17 10:58:39 +0300

    Adding a dynamic creation of deployment script.

--- functions/overcloud_deploy.sh
+++ functions/overcloud_deploy.sh
@@ -4,17 +4,27 @@ overcloud_deploy ()
     USE_CEPH=""
     USE_PANKO=""
 
-    if [ ! -z "$ceph_NUM" ] || [ $ceph_NUM -eq 0 ]
+    if [ $ceph_NUM -gt 0 ]
     then
-        USE_CEPH="--ceph-storage-scale $ceph_NUM --ceph-storage-flavor ceph-storage -e $THT/environments/storage-environment.yaml -e ./templates/ceph.yaml"
+        USE_CEPH="-e $THT/environments/storage-environment.yaml -e ./templates/ceph.yaml"
     fi
 
-    if [ $OS_VER -lt 11 ]
+    if [ $OS_VER -gt 10 ]
     then
         USE_PANKO="-e $THT/environments/services/panko.yaml"
     fi
 
+    # Starting index from 1, as 0 is Undercloud and no deployment parameters
+    # are needed.
+    for (( index=1; index<${#NODES[@]}; index++ ))
+    do
+        eval NAM=\$${NODES[$index]}_NAM
+        eval FLV=\$${NODES[$index]}_FLV
+        eval NUM=\$${NODES[$index]}_NUM
 
+        SCALES="$SCALES --${FLV}-scale $NUM "
+        FLAVORS="$FLAVORS --${FLV}-flavor $FLV "
+    done
 
     echo "Running the overcloud deployment."
     cat > deploy <<EOF
@@ -23,17 +33,17 @@ source stackrc
 openstack overcloud deploy \\
     --templates \\
     --libvirt-type kvm \\
-    --control-scale $controller_NUM --control-flavor control \\
-    --compute-scale $compute_NUM --compute-flavor compute \\
     --ntp-server $NTP \\
+    $SCALES \\
+    $FLAVORS \\
+    $USE_PANKO \\
+    $USE_CEPH \\
     -e ./templates/swap_env.yaml \\
     -e $THT/environments/services/sahara.yaml \\
-    $USE_PANKO \\
     -e $THT/environments/cinder-backup.yaml \\
     -e $THT/environments/network-environment.yaml \\
     -e $THT/environments/net-multiple-nics.yaml \\
     -e $THT/environments/network-isolation.yaml \\
-    $USE_CEPH \\
     -e ./templates/overrides.yaml > overcloud_deploy.log &
 EOF
     run_script_file deploy stack $HOST /home/stack/

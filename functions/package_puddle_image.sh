94bc52fd16b573dd8a826c2abc0946699e7011f9
Amit Ugol <amit.ugol@gmail.com>
2018-04-25 10:34:33 +0300

    Packaging issue fixed

--- functions/package_puddle_image.sh
+++ functions/package_puddle_image.sh
@@ -3,7 +3,7 @@ package_puddle_image ()
     PUDDLE=$(cat puddle)
     RR_CMD=$(cat rr_cmd)
     mkdir "$PUDDLE"
-    mv "$GUEST_IMAGE $PUDDLE"
+    mv "$GUEST_IMAGE" "$PUDDLE"
     echo "built on: $(date)" > "$PUDDLE"/version
     echo "command used: rhos-relese $RR_CMD" >> "$PUDDLE"/version
     echo "puddles used:" >> "$PUDDLE"/version

65e0d00c7b9a981046f894d7b154ea15637136f4
Amit Ugol <amit.ugol@gmail.com>
2018-09-05 08:04:57 +0300

    Fixed missing ending line.

--- functions/discover_puddle_version.sh
+++ functions/discover_puddle_version.sh
@@ -10,6 +10,21 @@ discover_puddle_version ()
             echo "$1" > puddle
             RR_CMD="${OS_VER} -p $1"
             echo "$RR_CMD" > rr_cmd
+            NAMESPACE=$(grep " namespace:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+            PREFIX=$(grep " prefix:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+            TAG=$(grep " tag:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+            CEPH_NAMESPACE=$(grep " ceph-namespace:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+            CEPH_IMAGE=$(grep " ceph-image:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+            CEPH_TAG=$(grep " ceph-tag:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
+
+            cat > docker_image_params <<EOF
+    --namespace $NAMESPACE \\
+    --prefix $PREFIX \\
+    --tag $TAG \\
+    --set ceph-namespace=$CEPH_NAMESPACE \\
+    --set ceph-image=$CEPH_IMAGE \\
+    --set ceph-tag=$CEPH_TAG \\
+EOF
         fi
     }
 
@@ -28,21 +43,6 @@ discover_puddle_version ()
         URL=$(cat puddle_dir_path)
         wget "$URL"/passed_phase2/overcloud_container_image_prepare.yaml
         PUDDLE=$(grep " id:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        NAMESPACE=$(grep " namespace:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        PREFIX=$(grep " prefix:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        TAG=$(grep " tag:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        CEPH_NAMESPACE=$(grep " ceph-namespace:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        CEPH_IMAGE=$(grep " ceph-image:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-        CEPH_TAG=$(grep " ceph-tag:" overcloud_container_image_prepare.yaml | awk '{print $NF}')
-
-        cat > docker_image_params <<EOF
-    --namespace $NAMESPACE \\
-    --prefix $PREFIX \\
-    --tag $TAG \\
-    --set ceph-namespace $CEPH_NAMESPACE \\
-    --set ceph-image $CEPH_IMAGE \\
-    --set ceph-tag $CEPH_TAG \\
-EOF
         set_puddle "$PUDDLE"
         echo "Using puddle: ${PUDDLE}."
         echo "rhos-release will be run with: $(cat rr_cmd)"

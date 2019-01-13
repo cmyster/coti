e2c8fab97db08cea4390bfc1cba60ec3919a42d5
Amit Ugol <amit.ugol@gmail.com>
2019-01-13 06:28:43 +0200

    Lowered CephPoolDefaultPgNum to 32

--- functions/install_host_packages.sh
+++ functions/install_host_packages.sh
@@ -2,7 +2,7 @@ install_host_packages ()
 {
     echo "Adding needed/wanted packages to the host."
     PACKS1="python-pyghmi python-pbr python-prettytable yum-utils"
-    PACKS2="elinks facter mc mlocate tree vim"
+    PACKS2="elinks facter mc mlocate tree vim wireshark-gnome"
     PACKS3="xorg-x11-apps xorg-x11-fonts-Type1 xauth"
     PACKS4="libvirt qemu-kvm virt-manager virt-install libguestfs-xfs libguestfs-tools-c"
     try "$PKG_CUST" install "$PACKS1 $PACKS2 $PACKS3 $PACKS4"  &> /dev/null || failure
